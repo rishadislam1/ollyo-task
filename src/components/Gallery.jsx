e1d8c9a0b15b224133fb7a101749779b6423303e
Rishad Islam <rishad.islam158@gmail.com>
2023-11-05 21:50:49 +0600

    checkbox done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,32 +1,38 @@
-
-
-import { useEffect } from 'react';
-import telephoneImage from '../assets/images/image-11.jpeg';
-import { useState } from 'react';
-import Product from './product';
-
+import { useEffect } from "react";
+import telephoneImage from "../assets/images/image-11.jpeg";
+import { useState } from "react";
+import Product from "./product";
 
 const Gallery = () => {
-    const [products,setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
-        fetch('product.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+  useEffect(() => {
+    fetch("product.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    console.log(products)
-
-    return (
-        <div className="mt-10 p-5">
-            <div className="grid grid-cols-5 justify-center items-center gap-10">
-                <img src={telephoneImage} alt="" className='rounded-xl border border-gray-400 col-span-2 row-span-2 cursor-pointer' />
-                {
-                    products.map(product=><Product key={product.id} product={product}></Product>)
-                }
-            </div>
+  return (
+    <div className="mt-10 p-5">
+      <div className="grid grid-cols-5 justify-center items-center gap-10 ">
+        <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 col-span-2 row-span-2">
+          <div>
+            <img
+              className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125 border border-gray-300"
+              src={telephoneImage}
+              alt=""
+            />
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         </div>
-    );
+
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,25 @@
+import { BiCheckbox, BiCheckboxChecked } from "react-icons/bi";
 
-const Product = ({product}) => {
-    const {id, image} = product;
-    return (
+const Product = ({ product }) => {
+  const { id, image } = product;
+  return (
+    <>
+      <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
         <div>
-            <img src={image} alt="" className='rounded-xl border border-gray-400 col-span-2 row-span-2 cursor-pointer' />
+          <img
+            className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125 border border-gray-300"
+            src={image}
+            alt=""
+          />
         </div>
-    );
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+
+        <div className="absolute inset-0 flex translate-x-[-90%] flex-col items-start justify-start  text-center transition-all duration-500 group-hover:translate-x-0">
+          <h1 className="font-dmserif text-3xl font-bold text-white"><BiCheckbox/><BiCheckboxChecked/></h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Product;
